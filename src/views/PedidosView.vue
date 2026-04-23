234df80aa68d30ceea1f35529a4e145ed9422beb
Caike Cunha <techcaikebr@gmail.com>
2026-04-23 09:53:21 -0300

    feat(dashboard): criar dashboard para gerenciamento de pedidos

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,16 +1,19 @@
 <template>
   <Banner />
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar pedidos</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
   import Banner from '@/components/Banner.vue';
+  import Dashboard from '@/components/Dashboard.vue';
 
   export default {
     components: {
       Banner,
+      Dashboard,
     }
   }
 </script>
