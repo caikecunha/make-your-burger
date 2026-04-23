c57a803a54ec2688fc39166f4ceb11dfa9d001d4
Caike Cunha <techcaikebr@gmail.com>
2026-04-23 10:27:28 -0300

    feat(dashboard): resgatar pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,15 +10,14 @@
         <div>Ações:</div>
       </div>
       <div id="burger-table-rows">
-        <div class="burger-table-row">
-          <div class="order-number">1</div>
-          <div>Usuário teste</div>
-          <div>Pão de Trigo</div>
-          <div>Alcatra</div>
+        <div class="burger-table-row" v-for="(burger, index) in burgers" :key="index">
+          <div class="order-number">{{ ++index }}</div>
+          <div>{{ burger.nome }}</div>
+          <div>{{ burger.pao }}</div>
+          <div>{{ burger.carne }}</div>
           <div>
             <ul>
-              <li>Salame</li>
-              <li>Tomate</li>
+              <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
             </ul>
           </div>
           <div>
@@ -36,6 +35,25 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    }
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burgers = data;
+    }
+  },
+  mounted() {
+    this.getPedidos();
+  }
 }
 </script>
 
